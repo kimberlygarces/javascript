8be2caf508584a7dd84a82375ce10b9cf1a56f9f
Kimberly Garcés <42309437+kimberlygarces@users.noreply.github.com>
2020-10-07 22:18:30 -0500

    Condicional múltiple: switch

---
+++ clase9.js
@@ -0,0 +1,65 @@
+var signo = prompt ('¿Cual es tu Horoscompo?')
+console.log(signo)
+
+switch (signo){
+
+    case 'Escorpión':
+        console.log('Alguien que te ama en secreto desde hace tiempo llega para flechar tu corazón ¡Cuidado! No dejes que el temor al dolor, te aleje de quien puede llegar a ser tu verdadero amor. Tu suerte y la culminación exitosa de tus proyectos depende únicamente de ti, así que con calma')
+
+    break
+    break
+
+    case'Sagitario':
+        console.log('No te detengas por ningún motivo frente a los obstáculos que se presentan para el desarrollo de tus proyectos, pues en estos momentos puedes fácilmente, en el campo que sea ¡Salir adelante! Tu participación en obras comunitarias, te llena de alegría y te da nuevos incentivos para poner en marcha tus ideas benefactoras.')
+    break
+
+    case'Capricornio':
+        console.log('Permanece todo este día optimista y encontrarás fácilmente soluciones a las distintas situaciones que debas enfrentar. No le temas a renovar recuerdos del pasado, de esta forma aclaras tus sentimientos y estabilizas tus emociones. El exceso de trabajo disminuyen tus capacidades y afectan tu salud, procura descansar para que tu espíritu reciba el alimento que necesita.')
+    break
+
+    case'Acuario':
+        console.log('Si te falta interés por sacar adelante tus planes, es posible que el desgano y el pesimismo se hayan apoderado de ti ¡Es hora de sacar fuerzas de donde no las tengas, para vencer este mal momento! Tu felicidad depende de la comprensión que le des a tu pareja y te amoldes a sus expresiones de carácter o de que luches por mantener tu individualidad.')
+    break
+
+    case'Piscis':
+        console.log('Llega un periodo largo de felicidad, gracias a la influencia de los astros sobre tu signo, pero debes prepárate para que algunos inconvenientes que aún tienes que pasar ¡No retarden tu dicha! Las actividades sociales te son necesarias para recobrar el ánimo, pero no te excedas, aún no es momento para descuidar la salud.')
+    break
+
+    case'Escorpión':
+        console.log('Llega un periodo largo de felicidad, gracias a la influencia de los astros sobre tu signo, pero debes prepárate para que algunos inconvenientes que aún tienes que pasar ¡No retarden tu dicha! Las actividades sociales te son necesarias para recobrar el ánimo, pero no te excedas, aún no es momento para descuidar la salud.')
+    break
+
+    case'Aries':
+        console.log('Marca un periodo en que enfrentarás el aumento tus responsabilidades laborales, así que es urgente que organices el tiempo para actuar con más calma en todo momento, para que esto no afecte tu rendimiento ni tu salud. ¡No te desesperes por la falta de dinero!')
+    break
+
+    case 'Tauro':
+        console.log('Es muy importante compartir hoy los momentos de triunfo y de alegría con tu familia, así les demuestras a tus seres queridos cuánto los aprecias y cuán valioso es para ti el amor que recibes de ellos. Aconsejan los astros hoy llenarte de pensamientos positivos para que actúes con mayor seguridad en todo lo que haces.')
+    break
+    case'Geminis':
+    case'Géminis':
+
+        console.log('El diálogo con el ser amado te llena de razones para continuar en pareja y te da seguridad para luchar por tus más grandes ideales. Los juegos de azar hoy no están favorecidos. Tienes la oportunidad de abrirte paso a nivel profesional aplicando tu inteligencia en aquello que realmente te gusta')
+    break
+    case'Cancer':
+        console.log('La falta de previsión en el manejo de la canasta familiar puede crearte problemas monetarios innecesarios ¡Pon más atención a esto y comienza a hacer ajustes! Deja que tu sensibilidad fluya para que puedas cubrir con la belleza de tu espíritu a quienes necesitan consuelo y apoyo para poder solucionar todos sus inconvenientes.')
+    break
+
+    case'Leo':
+        console.log('Día especial para permanecer alegre y contagiar de positivismo a los demás. Debes ser más cariñoso con quien te ama, pues necesita de tus manifestaciones de afecto para mantener la seguridad en ti. Muchos admiran tu capacidad de trabajo e iniciativa, pero es momento de reflexionar esto cuánto está afectando tu salud')
+    break
+
+    case'Virgo':
+        console.log('No te alejes de tus buenos amigos, ellos siempre han estado a tu lado en los buenos momentos y también en los difíciles. La meditación despeja tus incertidumbres y te hace más seguro y positivo. Cuida tu salud, el exceso de exposición al sol o al frío intenso, puede afectarte seriamente.')
+    break
+
+
+    case'Libra':
+        console.log('El amor hoy te trae buenas y nuevas sorpresas ¡Procura disfrutarlas al máximo! Durante estos días el campo laboral puede brindarte la posibilidad que estabas buscando para radicarte en otro país ¡Evalúa si es el mejor momento para esto!')
+    break
+
+    default:
+        console.log('No es su signo zodiacal')
+    break
+
+}
