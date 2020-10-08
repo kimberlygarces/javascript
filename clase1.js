4184cce562ecfac635cd72d49018f88dadd22821
Kimberly Garcés <42309437+kimberlygarces@users.noreply.github.com>
2020-10-07 21:43:25 -0500

    Variables

--- clase1.js
+++ clase1.js
@@ -6,8 +6,10 @@ var apellidoEnMinuscula = apellido.toLowerCase()
 
 var primerLetraDelNombre = nombre.charAt(0)
 
+
 var cantidadDeLetrasDelNombre = nombre.length
 
 var nombreCompleto = `${nombre} ${apellido}`
+
 
 var srt = nombre.substr(1,2)
