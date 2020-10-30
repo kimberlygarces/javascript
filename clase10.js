27eee07169e9b51939278eb105708d646171e18f
Kimberly Garcés <42309437+kimberlygarces@users.noreply.github.com>
2020-10-30 15:17:49 -0500

    Reducir un array a un valor (reduce)

--- clase10.js
+++ clase10.js
@@ -1,48 +1,50 @@
 var sacha = {
     nombre: 'Sacha',
     apellido: 'Lifszyc',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 111
 }
 
 var alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    cantidadDeLibros: 78
 }
 
 var martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 132
 }
 
 var dario = {
     nombre: 'Dario',
     apellido: 'Juarez',
-    altura: 1.71
+    altura: 1.71,
+    cantidadDeLibros: 90
 }
 
 var vicky = {
     nombre: 'Vicky',
     apellido: 'Zapata',
-    altura: 1.56
+    altura: 1.56,
+    cantidadDeLibros: 91
 }
 
 var paula = {
     nombre: 'Paula',
     apellido: 'Barros',
-    altura: 1.76
+    altura: 1.76,
+    cantidadDeLibros: 182
 }
-const esBajo = personas => personas.altura < 1.7
-const esAlta = persona => persona.altura > 1.8
-// OTRA FORMA DE ESCRIBIRLO
-// const esAlta = ({altura}) => altura > 1.8
 
-var personas= [sacha, alan, martin, dario, vicky, paula]
+const esAlta = ({ altura }) => altura > 1.8
 
-var personasAlatas = personas.filter(esAlta)
-var personasBajas = personas.filter(esBajo)
+var personas = [sacha, alan, martin, dario, vicky, paula]
 
+var personasAltas = personas.filter(esAlta)
 
 //  for (var i = 0; i < personas.length; i++)
 //  {
@@ -53,11 +55,26 @@ var personasBajas = personas.filter(esBajo)
 // array para filtar
 
 const pasarAlturaACms = persona => ({
-    ...persona,
-    altura: persona.altura*=100
+  ...persona,
+  altura: persona.altura * 100
 })
 
+var personasCms = personas.map(pasarAlturaACms)
 
- var personasCms = personas.map(pasarAlturaACms)
+// console.log(personasCms)
 
-//  console.log(personasCms)
+// var acum = 0
+
+// for (var i = 0; i < personas.length; i++){
+//     acum = acum + personas[i].cantidadDeLibros
+// }
+
+// console.log(`En total todos tienen ${acum} libros`)
+
+// OTRA FORMA DE HACERLO
+const reducer = (acum, {cantidadDeLibros}) => 
+            acum + cantidadDeLibros
+
+var totalDeLibros = personas.reduce(reducer, 0)
+
+console.log(`En total todos tienen ${totalDeLibros} libros`)
