2cd28b809b0eafaa6525e0dc6244aba8b2b6b17d
Kimberly Garcés <42309437+kimberlygarces@users.noreply.github.com>
2020-11-03 16:15:41 -0500

    Callbacks orden de obtección la respuesta del server

--- clase12-Callbacks.js
+++ clase12-Callbacks.js
@@ -1,15 +1,30 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
-
-
-const lukerUrl = `${API_URL}${PEOPLE_URL.replace(':id' , 1)}`
 const opts = {crossDomain: true}
 
-const onPeopleResponse = function(persona){
-    console.log(`Hola yo soy ${persona.name}`)
 
+function obtenerPersonaje(id, callback){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id' , id)}`
+    
+    $.get(url, opts, function(persona){
+        console.log(`Hola yo soy ${persona.name}`)
+
+        if(callback){
+            callback()
+        }
+    })
 }
 
 
-$.get(lukerUrl, opts, onPeopleResponse)
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3, function(){
+            obtenerPersonaje(4, function(){
+                obtenerPersonaje(5)
+            })
 
+        })
+
+    })
+
+})
